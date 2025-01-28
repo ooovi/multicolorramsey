afa1ffff34fe9c5b1e24dc5dc374ca1afb4480b8
ovi <ovi@posteo.de>
2025-01-28 12:18:11 +0100

    adding some stuff

--- MulticolorRamsey/Basic.lean
+++ MulticolorRamsey/Basic.lean
@@ -1,1 +1,10 @@
-def hello := "world"
+import Mathlib.Combinatorics.SimpleGraph.Basic
+
+namespace SimpleGraph
+
+variable {V : Type α} (G : SimpleGraph V)
+
+/-- An edge coloring maps each member of the graph's edge set to a colour in `C` --/
+abbrev EdgeColoring (C : Type) := G.edgeSet → C
+
+end SimpleGraph
